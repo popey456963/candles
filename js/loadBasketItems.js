63bb1be338e708d69b7c85a52db75ee1e4bf9dd5
Popey Gilbert <popey@debenclipper.com>
2015-09-21 10:21:29 +0100

    Basket + Fix First User Issue

--- js/loadBasketItems.js
+++ js/loadBasketItems.js
@@ -1,18 +1,23 @@
 function loadBasketItems() {
-	element = document.getElementById("basketStuff");
+
+	element = document.getElementById("tableDiv");
 	element.innerHTML = ""
 
-	console.group("Display-Basket");
+	console.groupCollapsed("Display-Basket");
 
 	console.log(items);
 
+	totalPrice = 0
+
+	tableData = "<table><thead><tr><th data-field='name'>Item Name</th><th data-field='price'>Item Price</th></tr></thead><tbody>"
 	for (i = 0; i < items.length; i++) {
 		console.log(items[i] + " = " + data[items[i]]);
-		line = items[i] + " = " + data[items[i]]
-		sellItem = "<button onclick='sellItem(items[i])'>Sell</button>"
-		linebreak = "<br /><br />"
-		element.innerHTML += line + sellItem + linebreak
+		tableData += "<tr><td>" + items[i] + " - " + data[items[i]] + "</td><td>£" + parseFloat(Math.round(String(parseFloat(prices[i]) * parseFloat(data[items[i]])) * 100) / 100).toFixed(2) + "</td></tr>"
+		totalPrice += parseFloat(prices[i]) * parseFloat(data[items[i]])
 	}
+	tableData += "<tr><td></td><td><b>Total: </b>£" + parseFloat(Math.round(totalPrice * 100) / 100).toFixed(2); + "</td></tr></tbody></table>"
+
+	element.innerHTML = tableData
 
 	console.groupEnd();
 }
--- js/persist-script.js
+++ js/persist-script.js
@@ -1,39 +1,60 @@
 var store = new Persist.Store("DataStore");
 
 var items = ['Scented Candle', 'Diffuser', 'Tea-Light']
+var prices = ['14.22', '12.11', '2.23']
 var data = {}
 
+
+function testFirstRun() {
+    console.groupCollapsed("Test Run");
+    for (i = 0; i < items.length; i++) {
+        var dataValue = items[i]
+        store.get(dataValue, function(ok, val) {
+            if (ok) {
+                console.log(dataValue + " = " + val);
+                if (val == "null") {
+                    store.set(items[i], 0);
+                }
+            }
+        });
+    }
+    console.groupEnd();
+}
+
+testFirstRun();
+
+
 function updateValues() {
-  var totalItems = 0
+    var totalItems = 0
 
-  console.group("Basket-Items");
+    console.groupCollapsed("Basket-Items");
 
-  for (i = 0; i < items.length; i++) {
-    var dataValue = items[i]
-    store.get(dataValue, function(ok, val) {
-      if (ok)
-        console.info(dataValue + ' = ' + val);
-      data[dataValue] = val;
-      totalItems = totalItems + parseInt(val);
-    });
-  }
-  console.info("Total Items = " + totalItems);
+    for (i = 0; i < items.length; i++) {
+        var dataValue = items[i]
+        store.get(dataValue, function(ok, val) {
+            if (ok)
+                console.info(dataValue + ' = ' + val);
+            data[dataValue] = val;
+            totalItems = totalItems + parseInt(val);
+        });
+    }
+    console.info("Total Items = " + totalItems);
 
-  document.getElementById("basketItems").innerHTML = totalItems;
+    document.getElementById("basketItems").innerHTML = totalItems;
 
-  console.groupEnd();
+    console.groupEnd();
 }
 
 updateValues();
 
 function buyItem(itemName) {
-  amount = parseInt(data[itemName]) + 1;
-  store.set(itemName, amount);
-  updateValues();
+    amount = parseInt(data[itemName]) + 1;
+    store.set(itemName, amount);
+    updateValues();
 }
 
 function sellItem(itemName) {
-  amount = parseInt(data[itemName]) - 1;
-  store.set(itemName, amount);
-  updateValues();
+    amount = parseInt(data[itemName]) - 1;
+    store.set(itemName, amount);
+    updateValues();
 }
